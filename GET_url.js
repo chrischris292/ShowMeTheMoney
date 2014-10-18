3e6a63da3e4abe37335b988ed43613c56e118bf8
William Xie <wxie6@illinois.edu>
2014-10-18 05:18:50 -0500

    GET_url essentials finished. New node modules added.

--- GET_url.js
+++ GET_url.js
@@ -1,8 +1,16 @@
+/* GET_url.js
+ * Script to get and output a list of the URLs for the given search query
+ * Output has to be saved via terminal (node GET_url.js > output.txt)
+ */
+
 // Dependencies
 var request = require("request");
+var fs = require("fs");
 var key = "f19eb237560f6772feeb6e955625f491:14:70018996"; // api key for article search
 // Adjust queries - do one at a time
 var search = "Tesla";
+var urls = [];
+var stream = fs.createWriteStream("url.txt");
 // var paging = ??
 
 function getData() {
@@ -15,12 +23,11 @@ function getData() {
 		if (error) {
 			throw error;
 		}
-		var urls = [];
 		var docs = data.response.docs;
 		for (var d in docs) {
-			urls.push(docs[d].web_url);
+			//urls.push(docs[d].web_url);
+			console.log(docs[d].web_url);
 		}
-		console.log(urls);
 	});
 }
 
