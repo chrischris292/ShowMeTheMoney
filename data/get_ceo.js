1bd27baa94c7bde57015bed8005c00e8c0c078ba
William Xie <wxie6@illinois.edu>
2014-10-18 13:20:10 -0500

    New company data retrieval script. Login page adjustments

--- data/get_ceo.js
+++ data/get_ceo.js
@@ -8,10 +8,22 @@ var fs = require("fs");
 // Adjust queries - do one at a time
 var search = "Tesla+Motors";
 
+function Glass(name, ceo, logo, homePage) {
+	this.name = name;
+	this.ceo = ceo;
+	this.logo = logo;
+	this.homePage = homePage;
+}
+
 function getData() {
 	var query = "http://api.glassdoor.com/api/api.htm?t.p=26106&t.k=g3q6WWopG8O&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q=" + search;
 	request(query, function(error, response, data) {
-		console.log(JSON.parse(data).response.employers[0].ceo.name);
+		var name = JSON.parse(data).response.employers[0].name;
+		var ceo = JSON.parse(data).response.employers[0].ceo.name;
+		var logo = JSON.parse(data).response.employers[0].squareLogo;
+		var homePage = JSON.parse(data).response.employers[0].website;
+		var company = new Glass(name, ceo, logo, homePage);
+		return company;
 	});
 }
 
