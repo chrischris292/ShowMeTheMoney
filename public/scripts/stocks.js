50c7cafe3855e1852dac80557d4d1255c0fd857f
chrischris292 <cbchan2@illinois.edu>
2014-10-18 19:15:18 -0400

    fixes

--- public/scripts/stocks.js
+++ public/scripts/stocks.js
@@ -43,7 +43,6 @@ function populateTopNews(unsortedData){
 		var newsDate = temp.date;
 		$("#newsFeedList").append("<li class = 'm-b-10'><span class = 'itemTitle'><a href = '"+url +"'>"+ title+"</a></span><small class = 'itemDate text-muted m-l-10'>"+newsDate + "</small></li>")
 	}
-<<<<<<< HEAD
 }
 
 function createGraph(unsortedData){
@@ -131,85 +130,7 @@ var	svg = d3.select("body")
 	svg.append("g")	
 		.attr("class", "y axis")
 		.call(yAxis);
-
-=======
-
-	var options = {
-
-	    ///Boolean - Whether grid lines are shown across the chart
-	    scaleShowGridLines : true,
-
-		//String - Colour of the grid lines
-    	scaleGridLineColor : "rgba(222,222,222,0.5)",
-
-	    //Number - Width of the grid lines
-	    scaleGridLineWidth : 1,
-
-	    //Boolean - Whether the line is curved between points
-	    bezierCurve : true,
-
-	    //Number - Tension of the bezier curve between points
-	    bezierCurveTension : 0.4,
-
-	    //Boolean - Whether to show a dot for each point
-	    pointDot : true,
-
-	    //Number - Radius of each point dot in pixels
-	    pointDotRadius : 4,
-    	scaleFontColor: "#fefefe",
-
-	    //Number - Pixel width of point dot stroke
-	    pointDotStrokeWidth : 1,
-
-	    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-	    pointHitDetectionRadius : 20,
-
-	    //Boolean - Whether to show a stroke for datasets
-	    datasetStroke : true,
-
-	    //Number - Pixel width of dataset stroke
-	    datasetStrokeWidth : 4,
-
-	    //Boolean - Whether to fill the dataset with a colour
-	    datasetFill : false,
-
-	    //String - A legend template
-	    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%='datasets[i].lineColor'%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-
-	};
-
-	var data = {
-	    labels: ["January", "February", "March", "April", "May", "June", "July"],
-	    datasets: [
-	        {
-		 		animationEasing: "easeOutBack",
-	            label: "My First dataset",
-	            fillColor: "#000",
-	            strokeColor: "yellow",
-	            //strokeColor: "rgba(220,220,220,1)",
-	            pointColor: "rgba(220,220,220,1)",
-	            pointStrokeColor: "#fff",
-	            pointHighlightFill: "#fff",
-	            pointHighlightStroke: "rgba(220,220,220,1)",
-	            data: [65, 59, 80, 81, 56, 55, 40]
-	        },
-	        {
-	            label: "My Second dataset",
-	            fillColor: "rgba(151,187,205,0.2)",
-	            strokeColor: "rgba(151,187,205,1)",
-	            pointColor: "rgba(151,187,205,1)",
-	            pointStrokeColor: "#fff",
-	            pointHighlightFill: "#fff",
-	            pointHighlightStroke: "rgba(151,187,205,1)",
-	            data: [28, 48, 40, 19, 86, 27, 90]
-	        }
-	    ]
-	};
-		
-	var ctx = document.getElementById("myChart").getContext("2d");
-	var myLineChart = new Chart(ctx).Line(data, options);
->>>>>>> FETCH_HEAD
-}
+	}
 
 function populateTopSentiment(sortedData){
 	for( i = 0;i<5;i++)
